c807b8b4ba1396fe28e4dcb2dcee30c457744cbb
= <kareem.force@gmail.com>
2024-03-29 01:57:33 +0200

    update relation

--- about/models.py
+++ about/models.py
@@ -18,8 +18,8 @@ class User(models.Model) :
     tall = models.DecimalField(max_digits = 4  , decimal_places = 2)
     registered_at = models.DateTimeField(default = datetime.now())
     profile = models.ImageField(upload_to = 'images/%y/%m/%d' , blank = True)
-    # many to one relation
-    dep = models.ForeignKey(Department , on_delete = models.CASCADE , null = True)
+    # many to one relation # related_name for the relation Department.users.all()
+    dep = models.ForeignKey(Department , related_name = 'users' , on_delete = models.CASCADE , null = True)
 
 
     def __str__(self) -> str:
